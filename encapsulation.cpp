40f5de608e5b30dc5584d44096431ff6b2c06b03
Prachi9974830195 <42738887+Prachi9974830195@users.noreply.github.com>
2019-01-24 15:02:56 +0530

    Update encapsulation.cpp

--- encapsulation.cpp
+++ encapsulation.cpp
@@ -8,17 +8,26 @@ class encapsulation
     
     public:
     void pub_fun(int acc_var);
+    
+    int get_var(void);
 
 };
 
 void encapsulation :: pub_fun(int acc_var)
 {
     pri_var = acc_var;
-    cout << "pri_var is : " << pri_var;
 }
+
+int encapsulation :: get_var(void)
+{
+   return pri_var; 
+}
+
 int main() 
 {
     encapsulation obj;
     
     obj.pub_fun(10);
+    
+    cout << "pri_var is : "<< obj.get_var();
 }
